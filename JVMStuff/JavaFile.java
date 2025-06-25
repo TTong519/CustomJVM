4f50ba28770e3d16c076929ecbcd00bce1d0ebab
Tyler Tong <tylertong519@gmail.com>
2025-06-25 12:39:04 -0700

    Add project files.

---
+++ JVMStuff/JavaFile.java
@@ -0,0 +1,11 @@
+public class JavaFile
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+    static int avg(int a, int b)
+    {
+        return (a + b);
+    }
+}
